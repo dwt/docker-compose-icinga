8398ef1cc6e1846d61dee9f724aaf8dbc1c3603c
Martin Häcker <mhaecker@mac.com>
2021-02-16 08:27:43 +0100

    Remove default passwords (they are now in an .env file)

--- init-icinga2.sh
+++ init-icinga2.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 if [ ! -f /data/etc/icinga2/conf.d/icingaweb-api-user.conf ]; then
-  sed "s/\$ICINGAWEB_ICINGA2_API_USER_PASSWORD/${ICINGAWEB_ICINGA2_API_USER_PASSWORD:-icingaweb}/" /config/icingaweb-api-user.conf >/data/etc/icinga2/conf.d/icingaweb-api-user.conf
+  sed "s/\$ICINGAWEB_ICINGA2_API_USER_PASSWORD/${ICINGAWEB_ICINGA2_API_USER_PASSWORD}/" /config/icingaweb-api-user.conf >/data/etc/icinga2/conf.d/icingaweb-api-user.conf
 fi
 
 if [ ! -f /data/etc/icinga2/features-enabled/icingadb.conf ]; then
